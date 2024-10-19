bbdf832f4182183dfcc62ba60f5f5999c3244376
Darina <darinax2005x@gmail.com>
2024-10-20 02:29:42 +0500

    Added book details

---
+++ js/bookDetails.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const readMoreButtons = document.querySelectorAll('.read-more-btn');
+
+    readMoreButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const bookDetails = this.nextElementSibling;
+            
+            if (bookDetails.style.display === "none") {
+                bookDetails.style.display = "block";
+                this.textContent = "Read Less";
+            } else {
+                bookDetails.style.display = "none";
+                this.textContent = "Read More";
+            }
+        });
+    });
+});
